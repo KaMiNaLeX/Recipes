2bb184f694ac98cf496bbcf099e7bbb5870d28ec
alkam <kaminskiy_2013@mail.ru>
2019-10-16 17:30:58 +0300

    second commit

---
+++ src/main/java/com/samsolutions/recipes/controllers/TestController.java
@@ -0,0 +1,13 @@
+package com.samsolutions.recipes.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("test")
+public class TestController {
+    @RequestMapping("/")
+    public String get() {
+        return "Hello world Hibernate2";
+    }
+}
