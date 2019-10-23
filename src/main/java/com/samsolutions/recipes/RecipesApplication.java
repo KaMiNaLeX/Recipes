dbd167cc29ba0481c7dd1bce40bef968c42c3279
alkam <kaminskiy_2013@mail.ru>
2019-10-23 16:34:26 +0300

    testpatch

--- src/main/java/com/samsolutions/recipes/RecipesApplication.java
+++ src/main/java/com/samsolutions/recipes/RecipesApplication.java
@@ -9,8 +9,8 @@ public class RecipesApplication {
     public static void main(String[] args) {
         SpringApplication.run(RecipesApplication.class, args);
         TestLog4j testLog4j = new TestLog4j();
-        //testLog4j.doSomething();
-        
+        testLog4j.doSomething();
+
     }
 
 }
