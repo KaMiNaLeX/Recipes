16fe0d7876062534a1f5591cbf80c81d2b5a8736
alkam <kaminskiy_2013@mail.ru>
2019-10-28 18:43:16 +0300

    add ProfileEntity

---
+++ src/main/java/com/samsolutions/recipes/models/ProfileEntity.java
@@ -0,0 +1,97 @@
+package com.samsolutions.recipes.models;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+/**
+ * @author kaminskiy.alexey
+ * @since 2019.10
+ */
+@Entity
+@Table(name = "profile", schema = "recipes")
+public class ProfileEntity {
+    private int id;
+    private String firstname;
+    private String lastname;
+    private String eMail;
+    private String login;
+    private String password;
+
+    @Id
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "firstname")
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    @Basic
+    @Column(name = "lastname")
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    @Basic
+    @Column(name = "e-mail")
+    public String geteMail() {
+        return eMail;
+    }
+
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
+
+    @Basic
+    @Column(name = "login")
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    @Basic
+    @Column(name = "password")
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProfileEntity that = (ProfileEntity) o;
+        return id == that.id &&
+                Objects.equals(firstname, that.firstname) &&
+                Objects.equals(lastname, that.lastname) &&
+                Objects.equals(eMail, that.eMail) &&
+                Objects.equals(login, that.login) &&
+                Objects.equals(password, that.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstname, lastname, eMail, login, password);
+    }
+}
