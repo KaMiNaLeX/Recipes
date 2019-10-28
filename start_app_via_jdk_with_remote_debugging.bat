1ad08e52317ff8901d0fabc974f5023cc5a1716a
alkam <kaminskiy_2013@mail.ru>
2019-10-28 17:07:12 +0300

    fix actuator

--- start_app_via_jdk_with_remote_debugging.bat
+++ start_app_via_jdk_with_remote_debugging.bat
@@ -1,4 +1,4 @@
 set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_144
 
 rem Source: https://docs.spring.io/spring-boot/docs/current-SNAPSHOT/reference/htmlsingle/#using-boot-running-as-a-packaged-application
-%java_home%\BIN\java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=5005,suspend=n -jar target/recipes-0.0.1-SNAPSHOT.jar
+%java_home%\BIN\java -jar -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 target/recipes-0.0.1-SNAPSHOT.jar
