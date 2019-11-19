156a18a58470291bfff3f6d7853b4efa0449b19e
KaMiNaLeX <kaminskiy_2013@mail.ru>
2019-11-19 11:18:57 +0300

    add bootstrap

--- src/main/java/com/samsolutions/recipes/config/WebSecurityConfig.java
+++ src/main/java/com/samsolutions/recipes/config/WebSecurityConfig.java
@@ -49,6 +49,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/").permitAll()
                 .antMatchers("/client/login").permitAll()
                 .antMatchers("/client/registration").permitAll()
+                .antMatchers("/webjars/**").permitAll()
                 .antMatchers("/admin/**").hasAuthority("ADMIN").anyRequest()
                 .authenticated().and().csrf().disable().formLogin()
                 .loginPage("/client/login").failureUrl("/client/login?error=true")
