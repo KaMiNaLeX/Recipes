66bb503b8a38eb687c71ec3287f8f4e5c672ad2c
alkam <kaminskiy_2013@mail.ru>
2019-12-09 19:16:19 +0300

    delete @JsonIgnore

--- src/main/java/com/samsolutions/recipes/model/RecipeEntity.java
+++ src/main/java/com/samsolutions/recipes/model/RecipeEntity.java
@@ -1,6 +1,5 @@
 package com.samsolutions.recipes.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.samsolutions.recipes.model.Enum.CookingDifficulty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -82,43 +81,4 @@ public class RecipeEntity extends BaseEntity {
     )
     private List<CategoryRecipeEntity> categoryRecipeEntities;
 
-    @JsonIgnore
-    public List<CategoryRecipeEntity> getCategoryRecipeEntities() {
-        return categoryRecipeEntities;
-    }
-
-    @JsonIgnore
-    public void setCategoryRecipeEntities(List<CategoryRecipeEntity> categoryRecipeEntities) {
-        this.categoryRecipeEntities = categoryRecipeEntities;
-    }
-
-    @JsonIgnore
-    public List<CookingStepsEntity> getCookingStepsEntityList() {
-        return cookingStepsEntityList;
-    }
-
-    @JsonIgnore
-    public void setCookingStepsEntityList(List<CookingStepsEntity> cookingStepsEntityList) {
-        this.cookingStepsEntityList = cookingStepsEntityList;
-    }
-
-    @JsonIgnore
-    public List<FavoriteEntity> getFavoriteEntityList() {
-        return favoriteEntityList;
-    }
-
-    @JsonIgnore
-    public void setFavoriteEntityList(List<FavoriteEntity> favoriteEntityList) {
-        this.favoriteEntityList = favoriteEntityList;
-    }
-
-    @JsonIgnore
-    public List<RecipeIngredientEntity> getRecipeIngredientEntityList() {
-        return recipeIngredientEntityList;
-    }
-
-    @JsonIgnore
-    public void setRecipeIngredientEntityList(List<RecipeIngredientEntity> recipeIngredientEntityList) {
-        this.recipeIngredientEntityList = recipeIngredientEntityList;
-    }
 }
