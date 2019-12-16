7d3570b68540e3747224d9f133557b0adac24a91
KaMiNaLeX <kaminskiy_2013@mail.ru>
2019-12-17 01:02:39 +0300

    update html

--- frontend/src/app/component/recipe-add/recipe-add.component.ts
+++ frontend/src/app/component/recipe-add/recipe-add.component.ts
@@ -3,6 +3,7 @@ import {ActivatedRoute, Router} from "@angular/router";
 import {RecipeService} from "../../service/recipe.service";
 import {CreateRecipeDTO} from "../../model/createRecipe/create-recipe-dto";
 import {IngredientRecipeDTO} from "../../model/createRecipe/ingredient-recipe-dto";
+import {CategoryRecipeDTO} from "../../model/createRecipe/category-recipe-dto";
 
 @Component({
   selector: 'app-recipe-add',
@@ -15,7 +16,7 @@ export class RecipeAddComponent implements OnInit {
   first = true;
   second = false;
   third = false;
-  ingredients: IngredientRecipeDTO[];
+  ingredients: IngredientRecipeDTO[] = [];
   ingredient: IngredientRecipeDTO = new IngredientRecipeDTO();
 
   constructor(private route: ActivatedRoute, private router: Router, private recipeService: RecipeService) {
@@ -31,7 +32,7 @@ export class RecipeAddComponent implements OnInit {
     this.third = false;
   }
 
-  toIngredient() {
+  toIngredient(name: string, difficulty: string, time: number, category: Array<CategoryRecipeDTO>) {
     this.first = false;
     this.second = true;
     this.third = false;
@@ -43,28 +44,27 @@ export class RecipeAddComponent implements OnInit {
     this.third = true;
   }
 
-  addIngredient() {
-    this.ingredient.name = "test";
-    this.ingredient.unit = "test";
-    this.ingredient.amount = 100;
-    this.ingredient.note = "";
+  addIngredient(name: string, amount: number, unit: string, note: string) {
+    this.ingredient.name = name;
+    this.ingredient.amount = amount;
+    this.ingredient.unit = unit;
+    this.ingredient.note = note;
+    this.ingredients.push(this.ingredient);
 
 
     let div = document.getElementById("ingredient");
+    let count = document.getElementsByName("ingredient").length;
     let div2 = div.cloneNode(true);
+    div.id += (count);
     div.parentNode.insertBefore(div2, div);
 
-    let count = document.getElementsByName("delete").length;
-    let deleteButton = document.getElementById("delete");
-    deleteButton.hidden = false;
-    let d = document.getElementsByName("delete")[count - 1];
-    d.hidden = true;
-
-    div.id = "ingredient" + (count - 1);
+    let emptyDiv = document.getElementById("ingredient1");
+    emptyDiv.hidden = true;
 
     document.getElementsByName("i.name")[0].textContent = this.ingredient.name;
     document.getElementsByName("i.amount")[0].textContent = this.ingredient.amount.toString();
     document.getElementsByName("i.unit")[0].textContent = this.ingredient.unit;
+    document.getElementsByName("i.note")[0].textContent = this.ingredient.note;
   }
 
   deleteIngredient() {
@@ -77,7 +77,7 @@ export class RecipeAddComponent implements OnInit {
     div.parentNode.insertBefore(div2, div);
 
     document.getElementsByName("c.name")[0].textContent = "test";
-    document.getElementsByName("c.description")[0].textContent = "test";
+    document.getElementsByName("c.description")[0].textContent = "lexaloil ooooooooooo ooooooooooooooooo ooooooooooooo ooooooooooooooo oooooooooooooo oooooooooooooooooooooooo ooooooooooooooooo ooooooooooooooooo oooooooooooooo oooooooooooo ooooooooo ooooox";
     document.getElementsByName("c.number")[0].textContent = "1";
   }
 
