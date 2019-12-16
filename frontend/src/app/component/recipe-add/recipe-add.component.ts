419ef3c5b9dbe15f7ccd08f4d5400b70e3299fec
alkam <kaminskiy_2013@mail.ru>
2019-12-16 18:28:36 +0300

    hidden div forms

--- frontend/src/app/component/recipe-add/recipe-add.component.ts
+++ frontend/src/app/component/recipe-add/recipe-add.component.ts
@@ -11,12 +11,33 @@ import {CreateRecipeDTO} from "../../model/createRecipe/create-recipe-dto";
 export class RecipeAddComponent implements OnInit {
 
   createRecipeDTO: CreateRecipeDTO;
+  first = true;
+  second = false;
+  third = false;
 
   constructor(private route: ActivatedRoute, private router: Router, private recipeService: RecipeService) {
     this.createRecipeDTO = new CreateRecipeDTO();
   }
 
   ngOnInit() {
+  }
+
+  toDescription() {
+    this.first = true;
+    this.second = false;
+    this.third = false;
+  }
+
+  toIngredient() {
+    this.first = false;
+    this.second = true;
+    this.third = false;
+  }
+
+  toCooking() {
+    this.first = false;
+    this.second = false;
+    this.third = true;
   }
 
 }
