bb0670b6cc554c1d618c151ae98169e9f424be7c
alkam <kaminskiy_2013@mail.ru>
2020-03-31 17:18:56 +0300

    update front view recipe

--- frontend/src/app/component/recipe-view/recipe-view.component.ts
+++ frontend/src/app/component/recipe-view/recipe-view.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {CreateRecipeDTO} from "../../model/createRecipe/create-recipe-dto";
 import {RecipeService} from "../../service/recipe.service";
 import {SharedService} from "../../service/shared.service";
+import {FavoriteService} from "../../service/favorite.service";
 
 @Component({
   selector: 'app-recipe-view',
@@ -11,16 +12,31 @@ import {SharedService} from "../../service/shared.service";
 export class RecipeViewComponent implements OnInit {
   createRecipeDTO: CreateRecipeDTO = new CreateRecipeDTO();
   ru: boolean;
+  dataSource: any;
+  dataSource2: any;
+  displayedColumns: string[] = ['ingredient', 'amount', 'unit', 'note'];
+  displayedColumns2: string[] = ['photo', 'description'];
+  authenticated = false;
 
-  constructor(private createRecipeService: RecipeService, private ss: SharedService) {
+  constructor(private createRecipeService: RecipeService, private ss: SharedService, private favoriteService: FavoriteService) {
+    if (localStorage.getItem('token') != undefined) {
+      this.authenticated = true;
+    }
   }
 
   ngOnInit() {
     this.createRecipeService.getById(sessionStorage.getItem('recipe')).subscribe(
-      data => this.createRecipeDTO = data);
+      data => {
+        this.createRecipeDTO = data;
+        this.dataSource = this.createRecipeDTO.ingredientRecipeDTOList;
+        this.dataSource2 = this.createRecipeDTO.cookingStepRecipeDTOList;
+      });
     this.ru = (localStorage.getItem('lang') == 'ru');
     this.ss.getEmittedValue()
       .subscribe(item => this.ru = item);
   }
 
+  addToFavorite(id: string) {
+    this.favoriteService.addToFavorite(id, this.authenticated);
+  }
 }
